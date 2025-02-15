dd857db046264a671fe1d69a1b929c97e54da2d0
beerappabvgp <beerappabharathb@gmail.com>
2025-02-15 20:35:34 +0530

    updtae api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -43,10 +43,56 @@ router.get("/:id", async (req, res) => {
 router.delete("/:id",  async (req, res) => {
     const { id } = req.params;
     const user = await User.findByIdAndDelete(id);
+    if (!user) {
+        res.status(400).json({
+            msg: "No user in the DB",
+            user: user
+        });
+    }
     res.status(200).json({
         msg: "user deleted successfully ... ",
         user: user
     });
 });
+
+
+router.put("/:id", async (req, res) => {
+    try {
+        console.log(req.body);
+        const { username, password, email } = req.body;
+        if (!username || !password || !email) {
+            return res.status(400).json({
+                "msg": "All fields are required ... "
+            });
+        }
+        if (typeof username !== "string") {
+            return res.status(400).json({
+                "msg": "username must be a string"
+            });
+        }
+        if (typeof email !== "string") {
+            return res.status(400).json({
+                "msg": "email must be a string"
+            });
+        }
+        if (typeof password !== "string") {
+            return res.status(400).json({
+                "msg": "password must be a string"
+            });
+        }
+
+        const { id } = req.params;
+
+        // I wanted to update data on the db 
+        const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+        return res.status(201).json({
+            "msg": "User updated successfully .... ",
+            user: user
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 export default router;
